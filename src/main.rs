818d1adbc325e94bef16392ed4916632466d2e33
sing <singeonzz@126.com>
2022-11-01 20:36:23 +0800

    Commit：variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+
+    println!("number is {}", x);
+    // 没有mut的话会提示不能对不可变变量进行二次赋值
+    x = 6;
+
+    println!("number is {}", x);
 }
